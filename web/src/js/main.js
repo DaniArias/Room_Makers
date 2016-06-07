cf41ce495cf672174a6334099ed268d0652d2b24
dani <bydan5501@gmail.com>
2016-06-07 12:14:26 +0200

    Ya esta casi

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -2,3 +2,27 @@ var $          = require('jquery');
 var foundation = require('../../node_modules/foundation-sites/dist/foundation.js');
 
 $(document).foundation();
+
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle();
+
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
